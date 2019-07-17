fee1df2eb088104712c0b1e45b33fa33d62b3cd4
chanhk-im <cfasd1875@gmail.com>
2019-07-18 01:24:41 +0900

    20190718A_Prototype

---
+++ SW Festival Proto ICH/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocity;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            Move();
+        }
+    }
+
+    private void Move()
+    {
+        float _movingX = Input.GetAxisRaw("Horizontal");
+        float _movingY = Input.GetAxisRaw("Vertical");
+
+        Vector2 _velocityVector = new Vector2(_movingX * velocity, _movingY * velocity) * Time.deltaTime;
+
+        transform.position = transform.position + new Vector3(_velocityVector.x, _velocityVector.y, transform.position.z);
+    }
+
+
+
+}
---
+++ SW Festival Proto ICH/Assets/Scripts/RoundManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundManager : MonoBehaviour
+{
+    public GameObject[] text1;
+    public Transform[] spawn1;
+
+    private int textArrLen1;
+
+    private int round = 1;
+    public int allRounds;
+
+    void Start()
+    {
+        textArrLen1 = text1.Length;
+        ShuffleArray(text1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (round == 1)
+        {
+            SpawnText(text1, spawn1);
+        }
+
+        round = 0;
+    }
+
+    private void SpawnText(GameObject[] _text, Transform[] _spawn)
+    {
+        int _textArrLen = _text.Length;
+
+        for (int i = 0; i < _textArrLen; i++)
+        {
+            Instantiate(_text[i], _spawn[i]);
+        }
+    }
+
+    public static void ShuffleArray<T>(T[] array)
+    {
+        int random1;
+        int random2;
+
+        T tmp;
+
+        for (int index = 0; index < array.Length; ++index)
+        {
+            random1 = UnityEngine.Random.Range(0, array.Length);
+            random2 = UnityEngine.Random.Range(0, array.Length);
+
+            tmp = array[random1];
+            array[random1] = array[random2];
+            array[random2] = tmp;
+        }
+    }
+
+}
