7844b8050f10de80336433310f4daa691b4a861c
chanhk-im <cfasd1875@gmail.com>
2019-07-19 10:38:23 +0900

    20190719B

---
+++ SW Festival Proto ICH/Assets/Scripts/TextManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextManager : MonoBehaviour
+{
+    private StageManager sm;  // stagemanager에 있는 destroyFlag 참조하기 위해 만듦
+
+    void Start()
+    {
+        sm = FindObjectOfType<StageManager>();
+    }
+
+
+    void Update()
+    {
+        if (sm.destroyFlag == true)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
